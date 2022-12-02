6d367be21cde997687f00d372a2c1bcb8ff858f1
Marcio Silva <marciosilvadrum@gmail.com>
2022-12-02 16:00:48 -0300

    post new album route done

--- routes/album.router.js
+++ routes/album.router.js
@@ -4,3 +4,18 @@ import AlbumModel from "../models/album.model.js";
 const albumRoute = express.Router();
 
 //ROUTES
+albumRoute.post("/create-album", async (req, res) => {
+  try {
+    const form = req.body;
+
+    const newAlbum = await AlbumModel.create(form);
+
+    return res.status(201).json(newAlbum);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
+
+//EXPORT
+export default albumRoute;
